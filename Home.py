6f6a126185101bdfab487ba57ef3f38c9f16e5e8
Alvin Cho <alvincho@jumbosoft.com>
2024-08-31 14:11:10 +0800

    change to home.py

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.write("# Welcome to OSMB! 👋")
+with open("home.md","r") as file:
+    st.write(file.read())
